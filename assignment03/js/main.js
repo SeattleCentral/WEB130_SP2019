61373c3e8245c6565bc215025480e57744c94549
EC2 Default User <ec2-user@ip-172-31-16-236.us-west-2.compute.internal>
2019-05-02 02:52:01 +0000

    Add jQuery code to open/close menu on mobile.

--- assignment03/js/main.js
+++ assignment03/js/main.js
@@ -1,5 +1,16 @@
 /* global $ */
 
-$('#menu').on('click', function(event) {
-    alert('You clicked me!')
+// $('li a').on('click', function(event) {
+//     const clickedEl = event.target
+//     $(clickedEl).css('color', 'pink');
+// })
+
+$('#menu').on('click', (event) => {
+    if ($('#menu > ul').hasClass('closed')) {
+        $('#menu > ul').removeClass('closed')
+        $('#hamburger').css('background-image', 'url("img/X-Icon.png")')
+    } else {
+        $('#menu > ul').addClass('closed')
+        $('#hamburger').css('background-image', 'url("img/Hamburger.png")')
+    }
 })
